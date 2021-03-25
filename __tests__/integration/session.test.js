d3ee2dbdba88c0abb49dc98fd1dfa3cd9892c0cd
Diogo Ferreira <arikbartzadok@gmail.com>
2021-03-25 01:07:07 -0300

    tests for auth with jwt and more

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,8 +1,7 @@
 const request = require('supertest')
+
 const app = require('../../src/app')
-
-const { User } = require('../../src/app/models')
-
+const factory = require('../factories')
 const truncate = require('../utils/truncate')
 
 describe('Authentication', () => {
@@ -13,11 +12,26 @@ describe('Authentication', () => {
     })
 
     it('should authenticate with valid credentials', async () => {
-        // Criando um novo usuário
-        const user = await User.create({
-            name: 'Diogo',
-            email: 'diogo.teste1@teste.com',
-            password_hash: '123000'
+        // Criando um novo usuário, sobrescrevendo a propriedade password do meu factory
+        const user = await factory.create('User', {
+            password: '123000'
+        })
+
+        console.log(user);
+
+        // Autenticando um novo usuário
+        const response = await request(app).post('/sessions').send({
+            email: user.email,
+            password: '123000'
+        })
+
+        expect(response.status).toBe(200)
+    })
+
+    it('should not authenticate with invalid credential', async () => {
+        // Criando um novo usuário, sobrescrevendo a propriedade password do meu factory
+        const user = await factory.create('User', {
+            password: '123000'
         })
 
         // Autenticando um novo usuário
@@ -26,6 +40,56 @@ describe('Authentication', () => {
             password: '11111111'
         })
 
+        expect(response.status).toBe(401)
+    })
+
+    it('should return jwt token when authenticated', async () => {
+        // Criando um novo usuário, sobrescrevendo a propriedade password do meu factory
+        const user = await factory.create('User', {
+            password: '123000'
+        })
+
+        // Autenticando um novo usuário
+        const response = await request(app).post('/sessions').send({
+            email: user.email,
+            password: '123000'
+        })
+
+        // Espero que exista este campo no corpo de resposta da requisição
+        expect(response.body).toHaveProperty("token")
+    })
+
+    it('should be able to access private routes when authenticated', async () => {
+        // Criando um novo usuário, sobrescrevendo a propriedade password do meu factory
+        const user = await factory.create('User', {
+            password: '123000'
+        })
+
+        // Autenticando se um novo usuário pode acessar rotas que necessitem de autenticação
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer ${user.generateToken()}`)
+
+        // Espero que exista este campo no corpo de resposta da requisição
         expect(response.status).toBe(200)
+    })
+
+    it('should not be able to access private routes without jwt token', async () => {
+        // Requisição sem token, nem autenticação
+        const response = await request(app)
+            .get('/dashboard')
+
+        // Espero que exista este campo no corpo de resposta da requisição
+        expect(response.status).toBe(401)
+    })
+
+    it('should not be able to access private routes with invalid jwt token', async () => {
+        // Requisição sem token, nem autenticação
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer tokenDeTeste1230000`)
+
+        // Espero que exista este campo no corpo de resposta da requisição
+        expect(response.status).toBe(401)
     })
 })
